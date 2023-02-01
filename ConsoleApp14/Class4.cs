5c3718ec58d83d499e0a76b9467fc0eadc74fcf7
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2023-02-01 16:04:47 +0530

    fuctions

---
+++ ConsoleApp14/Class4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Class4
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
