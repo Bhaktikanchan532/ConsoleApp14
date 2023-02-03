62d63b6889999968887d05dfd6cfdff365f6c9d3
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2023-02-03 12:04:28 +0530

    homework

--- ConsoleApp14/Class6.cs
+++ ConsoleApp14/Class6.cs
@@ -12,17 +12,17 @@ namespace ConsoleApp14
         static void Main(string[] args)
         {
             //using Stack
-            Stack stack = new Stack();
-            stack.Push(10);
-            stack.Push(20);
-            stack.Push(30);
-            stack.Pop();
+            //Stack stack = new Stack();
+            //stack.Push(10);
+            //stack.Push(20);
+            //stack.Push(30);
+            //stack.Pop();
 
-            Console.WriteLine($"first element in the stack:{stack.Peek()}");
-            foreach (var item in stack)
-            {
-                Console.WriteLine(item);
-            }
+            //Console.WriteLine($"first element in the stack:{stack.Peek()}");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
 
 
@@ -52,6 +52,55 @@ namespace ConsoleApp14
             //}
 
 
+
+            ////using SortedList
+            //SortedList sl = new SortedList();
+            //sl.Add(10,"IND");
+            //sl.Add(1,"AUS");
+            //sl.Add(30, "USA"); 
+            //sl.Add(80, "UK");
+
+            //Console.WriteLine($"total elements:"+sl.Count);
+            //foreach (DictionaryEntry item in sl)
+            //{
+            //    Console.WriteLine(item.Key+" "+item.Value);
+            //}
+
+            ////SortedList ex2 add contact and name in sorted list
+            //SortedList sortedList = new SortedList();
+            //sortedList.Add(8551080100, "xxx");
+            //sortedList.Add(8888532532,"yyy");
+            //sortedList.Add(8881532532, "zzz");
+            //sortedList.Add(9549532532, "ppp");
+
+            //Console.WriteLine($"total elements:" + sortedList.Count);
+            //foreach (DictionaryEntry item in sortedList)
+            //{
+            //    Console.WriteLine(item.Key + " " + item.Value);
+            //}
+
+
+            //create an arraylist add number into it and use sort() and Revere()
+            ArrayList mylist= new ArrayList();  
+            mylist.Add(10);
+            mylist.Add(25);
+            mylist.Add(85);
+            mylist.Add(35);
+            mylist.Add(70);
+            mylist.Add(65);
+            Console.WriteLine("ArrayList before sort:");
+            foreach(int item in mylist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ArrayList after sort:");
+           mylist.Sort();
+            mylist.Reverse();
+            foreach (int item in mylist)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
